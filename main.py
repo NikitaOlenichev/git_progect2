ff949a7116a83d4ba02e34aa9300df1450d44498
Nikita Olenichev <2020nikol@mail.ru>
2022-12-01 15:28:49 +0300

    Second commit

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ def main_function():
     print('My second git program')
     print('And I change it every day')
 
+def second_function():
+    print('16 декабря - самый лучший день!')
+
 
 if __name__ == '__main__':
     main_function()
